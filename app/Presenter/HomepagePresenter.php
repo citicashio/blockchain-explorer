4f254c709eb3c8148afc38d003982a60d470ebf1
Olda Salek <mzk@mozektevidi.net>
2018-05-25 16:45:00 +0200

    tests

--- app/Presenter/HomepagePresenter.php
+++ app/Presenter/HomepagePresenter.php
@@ -2,9 +2,18 @@
 
 namespace App\Presenters;
 
+use App\Models\RpcDaemon;
 use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	public function renderDefault()
+	{
+		$rpcDaemon = new RpcDaemon('http://192.168.24.126', 19834);
+		$height = $rpcDaemon->getHeight();
+		dump($height);
 
+		$block = $rpcDaemon->getBlockByHeight($height-1);
+		dump($block);
+	}
 }
