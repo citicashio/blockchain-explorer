e56784346b68f4a84fcb6a188fe6870c529df2cc
Olda Salek <mzk@mozektevidi.net>
2018-07-27 09:32:47 +0200

    add more informations from response to TransactionData

--- app/Models/TransactionData.php
+++ app/Models/TransactionData.php
@@ -22,7 +22,11 @@ class TransactionData
 		//dump($response);
 		$data = $response->txs[0]->as_json;
 		$data = Json::decode($data);
-		//dump($data);
+		$data->block_height = $response->txs[0]->block_height;
+		$data->in_pool = $response->txs[0]->in_pool;
+		$data->output_indices = $response->txs[0]->output_indices;
+		$data->tx_hash = $response->txs[0]->tx_hash;
+		dump($data);
 
 		$blockData = new TransactionData();
 		$blockData->data = $data;
