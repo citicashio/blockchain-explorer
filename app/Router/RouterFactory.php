f0c9a540c5ce9ee39e247fb2c1939cbca73238a7
Olda Salek <mzk@mozektevidi.net>
2018-08-22 14:09:16 +0200

    remove index.php from url

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -15,6 +15,7 @@ class RouterFactory
 	public static function createRouter(): IRouter
 	{
 		$router = new RouteList();
+		$router[] = new Route('index.php', 'Homepage:default', IRouter::ONE_WAY);
 		$router[] = new Route('/block/<hash>', 'Homepage:detail');
 		$router[] = new Route('/height/<height>', 'Homepage:detailByHeight');
 		$router[] = new Route('/tx/<hash>', 'Homepage:transaction');
